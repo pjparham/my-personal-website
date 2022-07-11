b53aabce49b4afdb5ad7a8545cc819ff7f051eec
Parker Parham <parkerparham@gmail.com>
2022-07-10 21:34:51 -0400

    Added more styling, assigned multiple classes and divs

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+document.getElementById("section-1").onclick = function() {
+  
+    document.getElementById("section-1").style.display = "none";
+
+}
+
