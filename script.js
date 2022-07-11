f78aac3746e12e7a1251e7b345f256a6ff47d0e6
Parker Parham <parkerparham@gmail.com>
2022-07-10 22:35:58 -0400

    Updated JSto add buttons to toggle text

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
+section = [...document.getElementsByClassName(`section`)]; //these brackets turn HTML Collection into
+for (var i = 0; i <section.length; i++) {
+    section[i].addEventListener('click', function() {
+        content = this.getElementsByClassName('content');
+        for (var j = 0; j < content.length; j++) {
+            content[j].classList.toggle('hide');
+        }
+    });
+} 
+/*
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This content added by JavaScript today";
 document.querySelector("body").appendChild(h2);
-
-document.getElementById("section-1").onclick = function() {
-  
-    document.getElementById("section-1").style.display = "none";
-
+function navBarClick() {
+    section = document.getElementsByClassName(`section`);
+    for (var i = 0; i <section.length; i++) {
+        section[i].addEventListener("click", function() {
+            content = this.getElementsByClassName('content');
+            for (var j = 0; j < content.length; j++) {
+                content[j].classList.toggle('hide');
+            }
+        });
+    }
 }
+navBarClick() 
 
+section = document.getElementsByClassName(`section`);
+console.log(section)
+for (var i = 0; i <section.length; i++) {
+    section[i].addEventListener('click', function() {
+        content = this.getElementsByClassName('content');
+        for (var j = 0; j < content.length; j++) {
+            content[j].classList.toggle('hide');
+        }
+    });
+}
+console.log("content")
+*/
